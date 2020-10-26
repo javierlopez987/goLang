5bd9ed5ac6794e703312e34f101a92cc6c3b6c59
Javier Lopez Ferreira <javierlopez987@gmail.com>
2020-10-26 01:28:16 -0300

    captura de error en funciones

--- src/intro/intro.go
+++ src/intro/intro.go
@@ -1,11 +1,20 @@
 package main
 
 import "fmt"
+import "errors"
 
 func suma(a, b int) (int, error) {
+	if a < b {
+		return 0, errors.New("el primer valor es menor que el segundo valor")
+	}
 	return a + b, nil
 }
 
 func main() {
-	fmt.Println(suma(1, 2))
+	r, err := suma(1, 2)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(r)
+	}
 }
