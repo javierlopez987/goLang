679352c6387102c5e37635ad30190c175ddb7339
Javier Lopez Ferreira <javierlopez987@gmail.com>
2020-11-27 20:27:12 -0300

    creacion y consulta con sqlite

---
+++ docs/sql.md
@@ -0,0 +1,4 @@
+# GoLang
+## Connecting to DB
+### SQLite
+- [sqlx]("jmoiron.github.io/sqlx/")
---
+++ src/practica/sql/sql.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main()  {
+	
+	var db *sqlx.DB
+
+	db, err := sqlx.Open("sqlite3", ":memory:")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	err = db.Ping()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	createSchema(db)
+	
+}
+
+// User ...
+type User struct {
+	ID int `db:"id"`
+	Name string `db:"name"`
+}
+
+func createSchema(db *sqlx.DB)  {
+	schema := `CREATE TABLE user (
+		id integer primary key autoincrement,
+		name varchar(56));`
+	_, err := db.Exec(schema)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	db.MustExec("INSERT INTO user (name) VALUES (?)", "Jane Doe")
+	rows, err := db.Query("SELECT id, name FROM user")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	for rows.Next() {
+		var u User
+		rows.Scan(&u.ID, &u.Name)
+		fmt.Println(u)
+	}
+}
